291a77922dd82bb0e01d223e4e1e7f0ee0d718c8
RedstoneAlmeida <mineps3marcos@gmail.com>
2016-04-12 13:19:58 -0300

    Update OneOne.php

--- OneOne/src/MyNET/OneOne.php
+++ OneOne/src/MyNET/OneOne.php
@@ -1,1 +1,16 @@
+<?php
 
+namespace MyNET/OneOne;
+
+use pocketmine\plugin\PluginBase;
+
+class OneOne extends PluginBase{
+  private $p1 = getPlayer(1);
+  private $p2 = getPlayer(2);
+  
+  private $oneone = $p1 + $p2;
+  
+  function onEnable(){
+    //Don't work
+  }
+}
