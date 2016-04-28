48d227cd2d514e160a3eac92a0e57f6857fbe3e1
Harris.com <megaspprt@gmail.com>
2016-04-29 01:50:35 +0800

    Update Main.php

--- DeathSound/src/DS/Main.php
+++ DeathSound/src/DS/Main.php
@@ -1,3 +1,37 @@
-<?php
+<?php 
 
-namespace KingOfTheLadder;
+namespace DeathSound;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\utils\TextFormat as color;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\level\sound\AnvilBreakSound;
+use pocketmine\level\sound\AnvilFallSound;
+use pocketmine\level\sound\AnvilUseSound;
+use pocketmine\level\sound\BatSound;
+use pocketmine\level\sound\BlazeShootSound;
+use pocketmine\level\sound\ClickSound;
+use pocketmine\level\sound\DoorBumpSound;
+use pocketmine\level\sound\DoorCrashSound;
+use pocketmine\level\sound\DoorSound;
+use pocketmine\level\sound\EndermanTeleportSound;
+use pocketmine\level\sound\FizzSound;
+use pocketmine\level\sound\GenericSound;
+use pocketmine\level\sound\GhastShootSound;
+use pocketmine\level\sound\GhastSound;
+use pocketmine\level\sound\LaunchSound;
+use pocketmine\level\sound\PopSound;
+use pocketmine\level\sound\ZombieHealSound;
+use pocketmine\level\sound\ZombieInfectSound;
+
+class Main extends PluginBase implements Listener{
+	
+	public function onEnable(){
+		$this->saveDefaultConfig;
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		$this->getServer()->getLogger()->info(color::BLUE. "Wow its has been activated!!");
+	}
+}
