f227c41e83d365e4701542e906cf5c44cdd71a9a
MyNET <megaco789@gmail.com>
2019-04-03 11:58:02 +0800

    Update Main.php

--- Gapple/src/Gapple/Main.php
+++ Gapple/src/Gapple/Main.php
@@ -16,14 +16,18 @@ class Main extends PluginBase implements Listener{
 	public function onEnable(){
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 		$this->getServer()->getLogger()->info(color::YELLOW. "Gapple by MyNET has been enable!");
+		$this->config = new Config($this->getDataFolder()."config.yml", Config::YAML, [
+			"health" => 40,
+			"max-health" => 40
+		]);
 	}
 	public function onEat(PlayerItemConsumeEvent $event){
 		$p=$event->getPlayer();
 		if($event->getItem()->getId() === 322){
             $p->addEffect(Effect::getEffect(10)->setAmplifier(3)->setDuration(200)->setVisible(true));
             $p->addEffect(Effect::getEffect(21)->setAmplifier(0)->setDuration(1000)->setVisible(true));
-                        $p->setHealth(40);
-			$p->setMaxHealth(40);
+            $p->setHealth($this->getConfig()->get("health"));
+			$p->setMaxHealth($this->getConfig()->get("max-health"));
 		}
 	}
 	public function onTip(PlayerItemHeldEvent $ev){
